b2bb1f8d192c2b597a52368275832220cc3709e8
Wade Liu <weichih654@gmail.com>
2016-11-30 01:52:17 +0800

    Using thread and queue to implement second search

--- bid_searcher.py
+++ bid_searcher.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import urllib2
+import urllib
 import re
 import time
 import logging
 import sys
+import Queue, time, threading, datetime
 
-logging.basicConfig (level=logging.DEBUG, filename="log")
+log = logging.getLogger ('bid')
+#hdlr = logging.StreamHandler ()
+log.setLevel (logging.DEBUG)
+#log.addHandler (hdlr)
 
 def get_content (url):
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -14,7 +19,7 @@ def get_content (url):
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive',
-       'Cookie': '_ts_id=3407350C3D053D053C0A; user_credit=45; good_credit=52; member=UrBHCGdZWdHjmvwkJuEVWHI6hNyx0C7IEFFlQ5c1mORT5KGB2qc6BDWmVkCXKqB1QoeJLv%2FOlJDeyq8sfKjPPLSFmdO7J%2Bbhm6aD%2FDk4VHfQNqDYiHNswgLh6da0gRE0fKU8%2Fv3DOlvnijIS%2FrRHvRS2g5t8pfUbZUWu2hlKUwBtmTA7IXHX80vL4%2FFGdXwtKIph%2BpecWjddb4cgv%2BqQ7XYyXsz30j%2BvTRxX%2BN%2B6S1MIu5imKwWYRFanmyvUByw%3D; bid_rid=5761590; bid_nick=5796c677; login=1; last_login_status=20161123103256+s+TW; login_status_code=1; rt_header_info=eyJ1c2VyX25pY2siOiJ3bGl1IiwiZ29vZF9jcmVkaXQiOjUyLCJsYXN0X2xvZ2luX3RpbWUiOjE0Nzk4NjgzNzYsImxhc3RfbG9naW5fY2hlY2siOnRydWV9; __asc=2f2fe5141589a01ff27709d2dd8; __auc=25f750cc15355afe2364d51e36d; _ga=GA1.3.213042389.1457431503'}
+       'Cookie': '_ts_id=340332053300390D3F0C; __gads=ID=75750d9ee6008bb4:T=1480304149:S=ALNI_MY6y901-6hRseczneJINR3Z2Ua5kA; __asc=d42340e6158a900c01d4c91956d; __auc=d42340e6158a900c01d4c91956d; _ga=GA1.3.1813721420.1480304149'}
     request=urllib2.Request(url,None,headers = hdr)
     try:
         response = urllib2.urlopen(request)
@@ -22,12 +27,11 @@ def get_content (url):
 
         html = data
     except:
-        logging.error ("get_content urlopen fail")
+        log.error ("get_content urlopen fail")
     return html
 
 class BidProduct:
     def __init__ (self):
-        logging.debug ("BidProduct construct")
         self.seller = None
         self.price = None
         self.current_price = None
@@ -49,6 +53,7 @@ class BidProduct:
 class BidSearcher:
     def __init__ (self, keyword, seller = None):
         logging.debug ("BidSearcher construct")
+        self.__products__ = []
 
     def __get_all_links (self, url):
         pass
@@ -58,6 +63,9 @@ class BidSearcher:
 
     def get_products (self):
         return self.__products__
+
+    def get_sellers (self):
+        pass
 
 class YahooSearcher(BidSearcher):
     def __init__ (self, keyword, seller = None):
@@ -72,19 +80,19 @@ class YahooSearcher(BidSearcher):
 class RutenSearcher(BidSearcher):
     def __init__ (self, keyword, seller = None):
         BidSearcher (keyword, seller)
-        self.__products__ = []
         self.html = None
-        self.max_page = 10
+        self.max_page = 5
         self.seller = seller
+        self.__sellers__ = []
 
         if self.seller is None:
             main_url = "http://search.ruten.com.tw/search/s000.php?enc=u&searchfrom=searchf&t=0&k="
         else:
             main_url = "http://search.ruten.com.tw/search/ulist00.php?s=" + seller + "&enc=b&c=0&k="
-            keyword = keyword.decode ('utf-8').encode ('big5')
+            keyword = urllib.quote_plus(keyword.decode ('utf-8').encode ('big5').replace('+', ' '))
 
         url = main_url + keyword
-        logging.debug ("url = %s" % url)
+        log.debug ("url = %s" % url)
         products = []
         for i in range (0, self.max_page):
             if self.seller is None:
@@ -94,21 +102,23 @@ class RutenSearcher(BidSearcher):
             if _products is None:
                 break;
             products.extend (_products)
+            if self.seller is not None:
+                break;
         #self.__get_all_links (url)
 
-        for p in products:
-            logging.debug ("all product link = %s, seller = %s" % (p.link, p.seller))
+        #for p in products:
+        #    log.debug ("all product link = %s, seller = %s" % (p.link, p.seller))
         self.__products__ = products
 
     def __get_all_links (self, url):
         if self.html == None:
             self.html = get_content (url)
-        #logging.debug ("html = %s" % self.html)
+        #log.debug ("html = %s" % self.html)
 
     def __get_products (self, url):
-        logging.debug ("get_products url = %s" % url)
+        log.debug ("get_products url = %s" % url)
         html = get_content (url)
-        #logging.debug ("html = %s" % html)
+        #log.debug ("html = %s" % html)
         if self.seller == None:
             m_link = re.findall ('<a class="item-name-anchor" href="(.*?)"', html)
         else:
@@ -130,11 +140,11 @@ class RutenSearcher(BidSearcher):
             return None
 
         if len (m_link) == 0:
-            logging.debug ("link is empty")
-            #logging.debug ("html = " + html)
+            log.debug ("link is empty")
+            #log.debug ("html = " + html)
             #return None
 
-        logging.debug ("link length = %d, name length = %d, seller length = %d" % (len (m_link), len (m_name), len (m_seller)))
+        log.debug ("link length = %d, name length = %d, seller length = %d" % (len (m_link), len (m_name), len (m_seller)))
 
         products = []
         if len (m_link) == len (m_name) and \
@@ -152,29 +162,112 @@ class RutenSearcher(BidSearcher):
                 i = i + 1
 
         for p in products:
-            logging.debug ("link = %s, title = %s" % (p.link, p.title))
+            log.debug ("link = %s, title = %s" % (p.link, p.title))
         return products
+
+    def get_sellers (self):
+        if len (self.__products__) == 0:
+            return []
+        all_sellers = []
+        for p in self.__products__:
+            all_sellers.append (p.seller)
+        self.__sellers__ = all_sellers
+        return set (self.__sellers__)
+
+def doJob(*args):
+    log.debug ("doJob")
+    queue = args[0]
+    while queue.qsize() > 0:
+        job = queue.get()
+        args[1][job.seller] = job.do()
+
+class Job:
+    def __init__ (self, seller, keyword):
+        self.seller = seller
+        self.keyword = keyword
+
+    def do (self):
+        log.debug ("Job do")
+        _s = RutenSearcher (self.keyword, self.seller)
+        _p = _s.get_products ()
+        log.debug ("Job done, seller = %s, keyword = %s, products count = %d" % (self.seller, self.keyword, len (_p)))
+        return _p
+
+def search_seller_by_all_keywords (searchers):
+    # Implement two searcher now
+    if len (searchers) != 2:
+        return None
+    s1 = RutenSearcher (searchers[0])
+    s2 = RutenSearcher (searchers[1])
+    p1 = s1.get_products ()
+    p2 = s2.get_products ()
+    log.debug ("p1 have %d products" % len (p1))
+    log.debug ("p2 have %d products" % len (p2))
+
+    hs1 = None
+    hs2 = None
+    if len (p1) < len (p2):
+        hs1 = s1
+        hs2 = s2
+        k1 = searchers[0]
+        k2 = searchers[1]
+    else:
+        hs1 = s2
+        hs2 = s1
+        k1 = searchers[1]
+        k2 = searchers[0]
+
+    sellers = hs1.get_sellers ()
+    sellers_count = len (sellers)
+
+    all_products = {}
+    for s in sellers:
+        all_products[s] = None
+
+    que = Queue.Queue()
+    for s in sellers:
+        que.put(Job(s, k2))
+
+    log.debug ("create Thd1")
+    thd1 = threading.Thread(target=doJob, name='Thd1', args=(que,all_products))
+    thd2 = threading.Thread(target=doJob, name='Thd2', args=(que,all_products))
+    thd3 = threading.Thread(target=doJob, name='Thd3', args=(que,all_products))
+    thd4 = threading.Thread(target=doJob, name='Thd4', args=(que,all_products))
+    thd5 = threading.Thread(target=doJob, name='Thd5', args=(que,all_products))
+    thd1.start()
+    thd2.start()
+    thd3.start()
+    thd4.start()
+    thd5.start()
+
+    all_finish = False
+    while all_finish is False:
+        all_finish = True
+        for p in all_products:
+            if all_products[p] is None:
+                all_finish = False
+                break
+        time.sleep (1)
+
+    _sellers = []
+    for p in all_products:
+        log.debug ("seller = %s, product count = %d" % (p, len (all_products[p]) if all_products[p] is not None else 0))
+        if len (all_products[p]) != 0:
+            _sellers.append (p)
+    return _sellers
+
+    #for s in sellers:
+    #    log.debug ("[%s]" % s)
+    #    _s = RutenSearcher (k2, s)
+    #    _p = _s.get_products ()
+    #    log.debug ("get %d products in second search" % len (_p))
+
+
 
 if __name__ == '__main__':
     print ("hello world\n")
     first_keyword = sys.argv[1]
     second_keyword = sys.argv[2]
 
-    searcher = RutenSearcher (first_keyword)
-    #searcher = Yahooearcher (first_keyword)
-    first_products = searcher.get_products()
-
-    all_sellers = []
-    for p in first_products:
-        all_sellers.append (p.seller)
-    all_sellers = set (all_sellers)
-
-    for a in all_sellers:
-        logging.info ("seller = %s" % a)
-        print ("seller = %s" % a)
-        searcher_2 = RutenSearcher (second_keyword, a)
-        products_2 = searcher_2.get_products ()
-        for s2 in products_2:
-            logging.info ("link = %s, title = %s, seller = %s", s2.link, s2.title, s2.seller)
-            print ("link = %s, title = %s, seller = %s, store = %s%s" % (s2.link, s2.title.decode ('big5').encode ('utf-8'), s2.seller, 'http://search.ruten.com.tw/search/ulist00.php?s=', s2.seller))
+    search_seller_by_all_keywords ([first_keyword, second_keyword])
 
